398ab5ffa87a2c1ad431ecf8dcde66139bf5176f
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2021-12-14 19:07:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
-pkg clean
-pkg install tty-clock -y
-pkg install sox -y
-pkg install jq -y
-pkg install figlet -y
-pkg install wget -y
-pkg install ffmpeg libwebp -y
-pkg install nodejs -y
-pkg install tesseract -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install tty-clock -y
+sudo apt-get install sox -y
+sudo apt-get install jq -y
+sudo apt-get install figlet -y
+sudo apt-get install wget -y
+sudo apt-get install ffmpeg
+sudo apt-get install libwebp -y
+sudo apt-get install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm audit fix
+npm i pm2 -g 
 
 #!/usr/bin/bash
 
