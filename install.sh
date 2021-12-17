640577da26f81977976bfd2beaa5d73a0b9cb524
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2021-12-18 04:07:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ pkg install sox -y
 pkg install jq -y
 pkg install figlet -y
 pkg install wget -y
-pkg install ffmpeg
 pkg install libwebp -y
 pkg install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
