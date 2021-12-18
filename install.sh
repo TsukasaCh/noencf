01ef4cbc1168e1466cc749181f36444d6fbd3669
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2021-12-18 18:52:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-pkg update -y
-pkg upgrade -y
-pkg install tty-clock -y
-pkg install sox -y
-pkg install jq -y
-pkg install figlet -y
-pkg install wget -y
-pkg install libwebp -y
-pkg install tesseract -y
+apt update -y
+apt upgrade -y
+apt install screenfetch -y
+apt install ffmpeg
+apt install tty-clock -y
+apt install sox -y
+apt install jq -y
+apt install figlet -y
+apt install wget -y
+apt install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install 
+npm audit fix
 npm i pm2 -g
 pm2 install ffmpeg 
  
@@ -19,6 +21,7 @@ pm2 install ffmpeg
 
 tty-clock -s -S -x -c -C 2 -b -B
 clear
+screenfetch
 pm2 start index.js
 pm2 save
 pm2 logs
